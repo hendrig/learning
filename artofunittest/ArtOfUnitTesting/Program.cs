a087bddb56001790910990a888f424f6eb3bac37
Hendrig Wernner Maus Santana Gonçalves <hendrig.goncalves@philips.com>
2020-07-10 10:55:42 -0300

    Commits the first code, about unit tests

---
+++ artofunittest/ArtOfUnitTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtOfUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ artofunittest/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,19 @@
+﻿using LogAn.Chapter2;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_Bad_Extension_ReturnsFalse()
+        {
+            LogAnalyzer log = new LogAnalyzer();
+
+            var result = log.IsValidLogFileName("invalid_extension.foo");
+
+            Assert.False(result);
+        }
+    }
+}
